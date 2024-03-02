d8ab936a48798e3a505ffbb87374926eed0425f5
ignaskmn <ignas@kmn.lt>
2024-03-02 21:48:07 +0200

    Add revalidate-layout API route and update Footer component

--- app/_components/Footer/index.tsx
+++ app/_components/Footer/index.tsx
@@ -1,9 +1,9 @@
 import type { Footer } from "@/app/_types/payload-types";
 import { fetchFooter } from "@/app/_api/fetchGlobals";
-import { PhoneIcon, EnvelopeIcon, MapPinIcon } from "@heroicons/react/24/solid";
+import { PhoneIcon, EnvelopeIcon, MapPinIcon} from "@heroicons/react/24/solid";
 import bandcamp from "@/app/_assets/social_icons/bandcamp.svg";
-import instagram from "@/app/_assets/social_icons/instagram.svg";
 import linkedin from "@/app/_assets/social_icons/linkedin.svg";
+import instagram from "@/app/_assets/social_icons/instagram.svg";
 import soundcloud from "@/app/_assets/social_icons/soundcloud.svg";
 import spotify from "@/app/_assets/social_icons/spotify.svg";
 import youtube from "@/app/_assets/social_icons/youtube.svg";
@@ -35,28 +35,34 @@ export default async function Footer() {
       <div className="flex flex-col md:flex-row gap-4 mx-4 md:ml-24 my-4 max-w-3xl">
         <div className="md:w-1/2 mb-8">
           <h3 className="text-4xl text-black mb-4">Contacts</h3>
-          <div className="flex items-center gap-2 text-lg">
-            <div className="w-4 h-4">
-              <EnvelopeIcon />
+          {footer?.email &&
+            <div className="flex items-center gap-2 text-lg">
+              <div className="w-4 h-4">
+                <EnvelopeIcon />
+              </div>
+              <a href={`mailto:${footer?.email}`} className="pt-1">
+                {footer?.email}
+              </a>
             </div>
-            <a href={`mailto:${footer?.email}`} className="pt-1">
-              {footer?.email}
-            </a>
-          </div>
-          <div className="flex items-center gap-2 text-lg">
-            <div className="w-4 h-4">
-              <PhoneIcon />
+          }
+          {footer?.phone &&
+            <div className="flex items-center gap-2 text-lg">
+              <div className="w-4 h-4">
+                <PhoneIcon />
+              </div>
+              <a href={`tel:${footer?.phone}`} className="pt-1">
+                {footer?.phone}
+              </a>
             </div>
-            <a href={`tel:${footer?.phone}`} className="pt-1">
-              {footer?.phone}
-            </a>
-          </div>
-          <div className="flex items-center gap-2 text-lg">
-            <div className="w-4 h-4">
-              <MapPinIcon />
+          }
+          {footer?.address &&
+            <div className="flex items-center gap-2 text-lg">
+              <div className="w-4 h-4">
+                <MapPinIcon />
+              </div>
+              <span className="pt-1">{footer?.address}</span>
             </div>
-            <span className="pt-1">{footer?.address}</span>
-          </div>
+          }
         </div>
         <div className="md:w-1/2">
           <div className="flex flex-row flew-wrap items-center gap-4 max-w-sm">
@@ -66,6 +72,7 @@ export default async function Footer() {
                 href={social.link}
                 target="_blank"
                 rel="noreferrer"
+                className="last:ml-12"
               >
                 <Image
                   src={social.type ? socialIcons[social.type] : ""}
