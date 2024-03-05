2840d9f606b445f2b2a21c1e39e2d5576583b3d6
ignaskmn <ignas@kmn.lt>
2024-03-05 21:01:26 +0200

    Remove unused code for design page and media components

--- app/(pages)/design/page.tsx
+++
@@ -1,7 +0,0 @@
-import ArchiveBlock from "@/app/_blocks/Archive";
-
-export default function Design() {
-    return <div className="flex items-center justify-center">
-        <ArchiveBlock />
-    </div>
-}
