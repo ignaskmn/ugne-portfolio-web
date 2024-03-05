b692a6bb4177f784b34020c87971c794545ad742
ignaskmn <ignas@kmn.lt>
2024-03-05 23:04:26 +0200

    Update image domains in next.config.js

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-        domains: ['localhost', 'https://admin.ugnemakselyte.com']
+        domains: ['localhost', 'admin.ugnemakselyte.com']
           .filter(Boolean)
           .map(url => url.replace(/https?:\/\//, '')),
       },
