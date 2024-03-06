bf6c8d2473cabdd4447530a10ea38949add8497f
ignaskmn <ignas@kmn.lt>
2024-03-06 15:06:31 +0200

    Update category margin in Card component

--- app/_components/Card/index.tsx
+++ app/_components/Card/index.tsx
@@ -5,7 +5,7 @@ export function Card({ title, image, slug, category }: Work) {
   return <div className="w-full block max-w-md border border-1 border-stone-400">
     <div className="p-4">
       <Media resource={image} />
-      <div className="w-full text-right"><h3 className="font-mono text-sm mt-1 px-1 border border-1 border-stone-400 inline-block">{category && typeof(category) === "object" ? category.title : ""}</h3></div>
+      <div className="w-full text-right"><h3 className="font-mono text-sm mt-2 px-1 border border-1 border-stone-400 inline-block">{category && typeof(category) === "object" ? category.title : ""}</h3></div>
       <h2 className="mt-1 text-xl">{title}</h2>
     </div>
   </div>
